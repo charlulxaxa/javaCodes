27b402630cf50c756f602f20b586d6f998a47e6f
charlulxaxa <charlulxaxa@gmail.com>
2025-09-01 21:44:29 +0800

    Song.java

--- Song.java
+++ Song.java
@@ -2,20 +2,47 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package musiclibrary;
+package music;
 
 /**
  *
  * @author charl
  */
-public class Song {
-     String Title;
-    String Artist;
-        Song(String title, String artist){
+public class Song extends Music{
+         String Title;
+         String Artist;
+         String Album;
+         String Genre;
+         int Year;
+        Song(String title, String artist,String album,String genre,int year){
             this.Title=title;
             this.Artist=artist;
+            this.Album=album;
+            this.Genre=genre;
+            this.Year=year;
         }
+        public String getTitle(){
+            return Title;
+        }
+        public String getArtist(){
+            return Artist;
+        }
+        public String getAlbum(){
+            return Album;
+        }
+        public String getGenre(){
+            return Genre;
+        }
+        public int getYear(){
+            return Year;
+        }
+        @Override
         public String toString(){
-            return Title +" by "+ Artist;
+            System.out.printf("%-20s %-20s%-20s%-15s%-15s",Title,Artist,Album,Genre,Year);
+            return "";
         }
 }
+
+
+
+
