a7f94a953c1d00da0f95d21c4fa35bd83f79e177
charlulxaxa <charlulxaxa@gmail.com>
2025-09-01 21:44:45 +0800

    Playlist.java

--- Playlist.java
+++ Playlist.java
@@ -2,15 +2,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package musiclibrary;
+package music;
 
 import java.util.*;
-public class Playlist {
-     String Name;
-    
+public class Playlist  {
+    String Name;
     ArrayList <Song> songs;
-    public int minutes = 2;
-    public int seconds = 59;
+    public int minutes = 0;
+    public int seconds = 10;
     
     Playlist(String name){
           this.Name = name;
@@ -27,14 +26,38 @@ public class Playlist {
          if(a>songs.size()){
                System.out.println("\nNo Song in number "+a+"\n");
           }else{
+              String songInfo=songs.get(a-1).getTitle()+" by "+songs.get(a-1).getArtist(); //store the song info since arrayList toString format is modified 
+              System.out.println("\n\n_____________________________________________________________________");
+              System.out.println(songInfo+" has been removed from the playlist");
+              System.out.println("_____________________________________________________________________\n\n");
               songs.remove(a-1);
-              System.out.println("\nSong removed\n");
+             
           }
       }
       public void displayList(){
-          System.out.println(Name);
-          for(Song s:songs){
-                  System.out.println(""+s+"    "+minutes+":"+seconds);
+        
+          System.out.println(songs.size());
+          System.out.println("\nSongs:");
+          System.out.printf("%-23s %-20s%-20s%-15s%-15s%-20s","Title","Artist","Album","Genre","Year","                    Time");
+          System.out.print("\n-------------------------------------------------------------------------------------------------------------------------------\n");
+          if(songs.size()==0){
+              System.out.println("\n                      ***EMPTY***                                ");
+          }
+          int numbering=1;
+          for(Song x:songs){
+                  System.out.print(numbering+") ");
+                  System.out.printf("%-20s%-1s%-1s%-1s",x,minutes,":",seconds); 
+                  if(songs.size()>0){
+                      System.out.println("");
+                  }
+                 
+                  numbering++;
               }  
+          
+          System.out.print("\n-------------------------------------------------------------------------------------------------------------------------------\n");
       }
 }
+       
+
+
+
